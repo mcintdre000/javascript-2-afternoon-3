4ab07709974acdb8a0c9176c8d763bbcb3619de8
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-05-02 17:54:02 -0700

    Day 2 - Callback Practice

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first(arr,cb){
+  cb(arr[0]);
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +51,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(arr,cb){
+  cb(arr[arr.length - 1]);
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,6 +72,9 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply(num1,num2,cb){
+  cb(num1*num2);
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +94,13 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(arr,name,cb){
+  for(let i=0;i<arr.length;i++){
+    if(arr[i] === name){
+      cb(true);
+    }else{cb(false)}
+  }
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +122,15 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq(arr,cb){
+  let uniqueArray = [];
+  for(let i =0;i<arr.length;i++){
+    if(uniqueArray.indexOf(arr[i]) == -1){
+      uniqueArray.push(arr[i]);
+    }
+  }
+  return cb(uniqueArray);
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -123,6 +148,11 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each(arr,cb){
+  for(let i=0;i<arr.length;i++){
+    cb(arr[i],i);
+  }
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -140,6 +170,14 @@ each(names, function(item, indice){
 */
 
 // Code here
+function getUserById(arr,id,cb){
+  for(let i=0;i<arr.length;i++){
+    if(arr[i]['id']===id){
+      //arr.find(arr[i])
+      cb(arr[i]);
+    }
+  }
+}
 
 // Do not edit the code below.
 var users = [
